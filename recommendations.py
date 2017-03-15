552ec75cd271e60a43b2d1c912280bccd0cfe2d6
Thien To Duc <to.duc.thien@framgia.com>
2017-03-15 13:42:16 +0700

    [recommendations] similarity Pearson metrics

--- recommendations.py
+++ recommendations.py
@@ -1,6 +1,6 @@
 # A dictionary of movie critics and their ratings of a small set of movies
 
-critics={
+critics = {
     'Lisa Rose': {
         'Lady in the Water': 2.5,
         'Snakes on a Plane': 3.5,
@@ -52,6 +52,7 @@ critics={
 }
 
 from math import sqrt
+import pdb
 
 # Return distance-based similarity score for person 1 and person 2
 def sim_distance(prefs, person1, person2):
@@ -69,3 +70,36 @@ def sim_distance(prefs, person1, person2):
                           for item in prefs[person1] if item in prefs[person2]])
 
     return 1/(1 + sum_of_squares)
+
+def sim_pearson(prefs, p1, p2):
+    #Get list of mutally rated items
+    si = {}
+    for item in prefs[p1]:
+        if item in prefs[p2]: si[item] = 1
+
+    #Find the number of elements
+    n = len(si)
+
+    # return 0 if they are no rating in common
+    if n == 0: return 0
+
+    '''Add up all the preferences'''
+    pdb.set_trace()
+    sum1 = sum([prefs[p1][it] for it in si])
+    sum2 = sum([prefs[p2][it] for it in si])
+
+    '''Sum up all the squares'''
+    sum1Sq = sum([pow(prefs[p1][it],2 ) for it in si])
+    sum2Sq = sum([pow(prefs[p2][it],2 ) for it in si])
+
+    '''Sum up the products'''
+    pSum = sum([prefs[p1][it]*prefs[p2][it] for it in si])
+
+    '''Calculate Pearson score'''
+    num = pSum - (sum1*sum2/n)
+    den = sqrt((sum1Sq - pow(sum1, 2)/n)*(sum2Sq - pow(sum2, 2)/n))
+    if den == 0: return 0
+
+    r = num/den
+
+    return r
