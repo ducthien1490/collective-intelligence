40eb8f25e5087262fe78b82952ae62d9cee47e8c
Thien To Duc <to.duc.thien@framgia.com>
2017-04-21 14:37:52 +0700

    User based recommendation

--- recommendations.rb
+++ recommendations.rb
@@ -75,3 +75,84 @@ def euclidean_distance data, ref_1, ref_2
 
   1/(1 + sum_squares_diff)
 end
+
+def pearson_correlation data, ref_1, ref_2
+  # return 0 if ref_1 or ref2_ not exist in data
+  return 0 unless data[ref_1] || data[ref_2]
+
+  #Get the common items
+  similar = []
+  data[ref_1].each do |item_key, item_val|
+    if data[ref_2][item_key]
+      similar.push item_key
+    end
+  end
+
+  #counting number of common items
+  item_num = similar.length
+
+  # return 0 if there're no common
+  return 0 if item_num == 0
+
+  #Add up all common item rating
+  sum_ref_1 = similar.map{|c_i| data[ref_1][c_i] }.sum
+  sum_ref_2 = similar.map{|c_i| data[ref_2][c_i] }.sum
+
+  #Add up all square of common item rating
+  sum_square_ref_1 = similar.map{|c_i| data[ref_1][c_i]**2 }.sum
+  sum_square_ref_2 = similar.map{|c_i| data[ref_2][c_i]**2 }.sum
+
+  #Sum all the products of common rating
+  products_sum = similar.map{|c_i| data[ref_1][c_i]*data[ref_2][c_i]}.sum
+
+  #Calculate Pearson Score
+  num = products_sum - sum_ref_1*sum_ref_2/item_num
+  den = Math.sqrt((sum_square_ref_1 - sum_ref_1**2/item_num)*
+                  (sum_square_ref_2 - sum_ref_2**2/item_num))
+  return 0 if den == 0
+  num/den
+end
+
+def top_matches data, user, n = 5
+  scores = data.map do |member, _|
+    next if user == member
+    #In this case, I use Pearson score
+    {}.tap do |member_rating|
+      member_rating[member] = pearson_correlation(data, user, member)
+    end
+  end.compact
+
+  #Sort the list to get the highest score
+  scores.sort_by{|user_rating| user_rating.values.first }.reverse.take(n)
+end
+
+def user_based_recommendation data, user
+  totals = {}
+  sim_sum = {}
+  data.each do |member, rating|
+    #Skip if member is user
+    next if member == user
+
+    sim_score = pearson_correlation(data, user, member)
+    next if sim_score <= 0
+    rating.each do |movie, rate|
+      #only score movies that user haven't seen yet
+      if data[user][movie].nil? || data[user][movie] == 0
+        #Similarity * score
+        totals[movie] = 0 if totals[movie].nil?
+        totals[movie] += (data[member][movie] * sim_score)
+
+        #Sum of similarities
+        sim_sum[movie] = 0 if sim_sum[movie].nil?
+        sim_sum[movie] += sim_score
+      end
+    end
+  end
+
+  rankings = totals.map do |re_movie, re_score|
+    {}.tap do |recommendation|
+      recommendation[re_movie] = totals[re_movie]/sim_sum[re_movie]
+    end
+  end
+  rankings.sort_by{|rank| rank.values.first }.reverse
+end
