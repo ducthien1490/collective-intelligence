65b5ab66684028e5920320b79e4c764d46127dd6
Thien To Duc <to.duc.thien@framgia.com>
2017-05-25 17:06:42 +0700

    Complete k-meaning cluster

--- clusters.py
+++ clusters.py
@@ -1,5 +1,7 @@
 from math import sqrt
 from cluster import bicluster
+from PIL import Image, ImageDraw
+import random
 import pdb
 
 def readfile(filename):
@@ -49,10 +51,10 @@ def hcluster(rows, distance=pearson):
         closest = distance(clust[0].vec, clust[1].vec)
 
         '''Loop through every pair looking for the smallest distance'''
-        for i in range(i + 1, len(clust)):
+        for i in range(len(clust)):
             for j in range(i+1, len(clust)):
                 '''distances is the cache of distance calculations'''
-                if(clust[i].id, clust(j).id) not in distances:
+                if(clust[i].id, clust[j].id) not in distances:
                     distances[(clust[i].id, clust[j].id)] = distance(clust[i].vec, clust[j].vec)
 
                 d = distances[(clust[i].id, clust[j].id)]
@@ -68,7 +70,7 @@ def hcluster(rows, distance=pearson):
         newcluster = bicluster(mergevec, left=clust[lowestpair[0]], right=clust[lowestpair[1]], distance=closest, id=currentclustid)
 
         '''cluster ids that weren't in the original set are negative'''
-        currentclustid = -1
+        currentclustid -= 1
         del clust[lowestpair[1]]
         del clust[lowestpair[0]]
         clust.append(newcluster)
@@ -89,3 +91,108 @@ def printclust(clust, labels=None, n=0):
     '''Now print the right and left branches'''
     if clust.left != None: printclust(clust.left, labels=labels, n=n+1)
     if clust.right != None: printclust(clust.right, labels=labels, n=n+1)
+
+def getheight(clust):
+    '''Return 1 if this is an endpoint'''
+    if clust.left == None and clust.right == None: return 1
+
+    '''Otherwise the height is the same of the height of each branch'''
+    return getheight(clust.left) + getheight(clust.right)
+
+def getdepth(clust):
+    '''The distance of endpoint is 0.0'''
+    if clust.left == None and clust.right == None: return 0
+
+    '''The distance of a branche is the greater of two sides plus ots own distance'''
+    return max(getdepth(clust.left), getdepth(clust.right)) + clust.distance
+
+def drawdendrogram(clust, labels, jpeg='clusters.jpg'):
+    '''height and width'''
+    h = getheight(clust) * 20
+    w = 1200
+    depth = getdepth(clust)
+
+    '''scaling'''
+    scaling = float(w - 150)/depth
+
+    '''Create a new image with white background'''
+    img = Image.new('RGB', (w,h), (255,255,255))
+    draw = ImageDraw.Draw(img)
+
+    draw.line((0, h/2, 10, h/2), fill=(255,0,0))
+
+    '''Draw the first node'''
+    drawnode(draw, clust, 10, (h/2), scaling, labels)
+    img.save(jpeg, 'JPEG')
+
+def drawnode(draw, clust, x, y, scaling, labels):
+    if clust.id < 0:
+        h1 = getheight(clust.left) * 20
+        h2 = getheight(clust.right) * 20
+        top = y - (h1 + h2)/2
+        bottom = y + (h1+h2)/2
+
+        '''Line length'''
+        ll = clust.distance*scaling
+
+        '''Vertical line from this cluster to children'''
+        draw.line((x, top+h1/2, x, bottom-h2/2), fill=(255,0,0))
+
+        '''Horizontal line to left item'''
+        draw.line((x, top+h1/2, x+ll, top+h1/2), fill=(255,0,0))
+
+        '''Horizontal line to right item'''
+        draw.line((x, bottom-h2/2, x+11, bottom-h2/2), fill=(255,0,0))
+
+        '''Call the function to draw the left and right nodes'''
+        drawnode(draw, clust.left, x+ll, top+h1/2, scaling, labels)
+        drawnode(draw, clust.right, x+ll, bottom-h2/2, scaling, labels)
+    else:
+        '''If this is an enpoint, draw the item label'''
+        draw.text((x+5, y-7), labels[clust.id], (0,0,0))
+
+def rotatematrix(data):
+    newdata = []
+    for i in range(len(data[0])):
+        newrow = [data[j][i] for j in range(len(data))]
+        newdata.append(newrow)
+    return newdata
+
+def kcluster(rows, distance=pearson, k=4):
+    '''Determine the minimum and maximum value for each point'''
+    ranges = [(min([row[i] for row in rows]), max([row[i] for row in rows]))
+              for i in range(len(rows[0]))]
+
+    '''Create k randomly placed centroids'''
+    clusters = [[random.random()*(ranges[i][1] - ranges[i][0]) + ranges[i][0]
+                 for i in range(len(rows[0]))] for j in range(k)]
+
+    lastmatches=None
+    for t in range(100):
+        print 'Iteration %d' %t
+        bestmatches = [[] for i in range(k)]
+
+        '''Find which centroid is the closest for each row'''
+        for j in range(len(rows)):
+            row = rows[j]
+            bestmatch = 0
+            for i in range(k):
+                d = distance(clusters[i], row)
+                if d < distance(clusters[bestmatch], row): bestmatch = i
+            bestmatches[bestmatch].append(j)
+
+        '''If the result are the same as last time, this is complete'''
+        if bestmatches == lastmatches: break
+        lastmatches = bestmatches
+
+        '''Move the centroids to the average of their members'''
+        for i in range(k):
+            avgs = [0.0]*len(rows[0])
+            if len(bestmatches[i]) > 0:
+                for rowid in bestmatches[i]:
+                    for m in range(len(rows[rowid])):
+                        avgs[m] += rows[rowid][m]
+                for j in range(len(avgs)):
+                    avgs[j] /= len(bestmatches[i])
+                clusters[i] = avgs
+    return bestmatches
